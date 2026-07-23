be0f153b021cf7627e5753783736d60574634462
SyedMahammadMustakhem <Syedmuhammadmustakhem@gmail.com>
2026-07-24 00:01:12 +0530

    fixed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 TapQR API running on http://localhost:${PORT}`);
+  console.log(`🚀 TapQR API IS  running on http://localhost:${PORT}`);
 });
